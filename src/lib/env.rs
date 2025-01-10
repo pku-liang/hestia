3e7aba302ea1f8b7810c2e583aed80b1b073a455
xuruifan <707185547@qq.com>
2025-01-10 12:11:10 +0800

    change mul_integer latency

--- src/lib/env.rs
+++ src/lib/env.rs
@@ -35,7 +35,7 @@ impl UnitEnv {
     pub fn new(unit: &Unit) -> Self {
         let latency = match &unit.op_type as &str {
             "register" => 1,
-            "mul_integer" => 3,
+            "mul_integer" => 2,
             "truncf" | "extf" | "sitofp" => 2,
             "div_integer" => 10,
             "sub_float" | "add_float" => match &unit.types[2] as &str {
